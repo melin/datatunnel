f2c02d4a11a629f03651c16b5e800696d835be14
binsong.li <binsong.li@tongdun.cn>
2021-08-04 15:32:30 +0800

    fix 添加SPI 注解

--- api/src/main/java/com/dataworker/datax/api/DataxReader.java
+++ api/src/main/java/com/dataworker/datax/api/DataxReader.java
@@ -1,5 +1,6 @@
 package com.dataworker.datax.api;
 
+import com.gitee.bee.core.extension.SPI;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
@@ -9,6 +10,7 @@ import java.util.Map;
 /**
  * @author melin 2021/7/27 10:47 上午
  */
+@SPI
 public interface DataxReader {
 
     void validateOptions(Map<String, String> options);
