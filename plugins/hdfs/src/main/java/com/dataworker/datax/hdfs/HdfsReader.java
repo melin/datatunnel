3cf303533a6145656a269804054fc2810693e094
binsong.li <binsong.li@tongdun.cn>
2021-08-19 11:05:43 +0800

    hdfs 支持 fileNameSuffix参数

--- plugins/hdfs/src/main/java/com/dataworker/datax/hdfs/HdfsReader.java
+++ plugins/hdfs/src/main/java/com/dataworker/datax/hdfs/HdfsReader.java
@@ -13,6 +13,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author melin 2021/7/27 11:06 上午
@@ -39,10 +40,22 @@ public class HdfsReader implements DataxReader {
         }
 
         String path = options.get("path");
-        Configuration configuration = sparkSession.sparkContext().hadoopConfiguration();
-        List<String> paths = Arrays.stream(FileSystem.get(configuration).listStatus(new Path(path)))
-                .map(file -> file.getPath().toString()).collect(Collectors.toList());
+        String pathPrefix = "/user/datawork/users/" + userId;
+        if (!StringUtils.startsWith(path, pathPrefix)) {
+            throw new DataXException("只能访问 " + pathPrefix + " 路径下文件");
+        }
 
+        String fileNameSuffix = options.get("fileNameSuffix");
+
+        Configuration configuration = sparkSession.sparkContext().hadoopConfiguration();
+        Stream<String> stream = Arrays.stream(FileSystem.get(configuration).listStatus(new Path(path)))
+                .map(file -> file.getPath().toString());
+
+        if (StringUtils.isNotBlank(fileNameSuffix)) {
+            stream = stream.filter(file -> StringUtils.endsWith(file, fileNameSuffix));
+        }
+
+        List<String> paths = stream.collect(Collectors.toList());
         return sparkSession.sqlContext().createDataset(paths, Encoders.STRING()).toDF();
     }
 }
