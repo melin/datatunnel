6839a1e55fa61a8acc81ead54fd7fac445d5c75b
miao.cai <miao.cai@tongdun.cn>
2021-08-30 23:34:39 +0800

    junit

--- plugins/hbase/src/test/java/com/dataworker/datax/hbase/HbaseWriterBulkLoadArrayZstd.java
+++ plugins/hbase/src/test/java/com/dataworker/datax/hbase/HbaseWriterBulkLoadArrayZstd.java
@@ -6,7 +6,6 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.security.UserGroupInformation;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.Function;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.RowFactory;
@@ -15,24 +14,27 @@ import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.Metadata;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
+import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 /****************************************
- * @@CREATE : 2021-08-28 3:40 下午
+ * @@CREATE : 2021-08-30 8:21 下午
  * @@AUTH : NOT A CAT【NOTACAT@CAT.ORZ】
  * @@DESCRIPTION :
  * @@VERSION :
  *****************************************/
-public class HbaseWriterTest {
-    public static void main(String[] args) throws Exception{
-//        create 'bulkLoadTest2', {NAME=>'f1', VERSIONS=>5},SPLITS => ['1', '3', '5', '7']
-//        get 'bulkLoadTest1','1',{COLUMN=>'f1',VERSIONS=>5}
+public class HbaseWriterBulkLoadArrayZstd {
+//        create 'bulkLoadTest4', {NAME=>'f1', VERSIONS=>5},SPLITS => ['1', '3', '5', '7']
+//        get 'bulkLoadTest4','1',{COLUMN=>'f1',VERSIONS=>5}
 
-        String table = "bulkLoadTest2";
-        String stagingDir = "20210828008";
+    @Test
+    public void bulkLoadArrayZstd() throws Exception{
+        String table = "bulkLoadTest4";
+        String stagingDir = "20210828009";
         String distCpstagingDir = "dist/" + stagingDir;
 
         String princ = "admin/admin@DZTECH.COM";
@@ -54,7 +56,7 @@ public class HbaseWriterTest {
         Map<String, String> map = new HashMap<>();
         map.put("table", table);
         map.put("writeMode", WriteMode.bulkLoad.name());
-        map.put("mappingMode", MappingMode.one2one.name());
+        map.put("mappingMode", MappingMode.arrayZstd.name());
         map.put("hfileDir", stagingDir);
         map.put("hfileTime", String.valueOf(System.currentTimeMillis()));
         map.put("hfileMaxSize", null);
@@ -77,19 +79,15 @@ public class HbaseWriterTest {
         list.add("zhaowu,4,5");
         JavaRDD<String> lines = jsc.parallelize(list);
 
-        JavaRDD<Row> rows = lines.map(new Function<String, Row>(){
-            private static final long serialVersionUID = -4332903997027358601L;
-            @Override
-            public Row call(String line) throws Exception {
-                String[] strArr = line.split(",");
-                String name = strArr[0];
-                Integer height = Integer.valueOf(strArr[1]);
-                Integer weight = null;
-                if (strArr.length == 3){
-                    weight = Integer.valueOf(strArr[2]);
-                }
-                return RowFactory.create(name, height, weight);
+        JavaRDD<Row> rows = lines.map((t)->{
+            String[] strArr = t.split(",");
+            String name = strArr[0];
+            Integer height = Integer.valueOf(strArr[1]);
+            Integer weight = null;
+            if (strArr.length == 3){
+                weight = Integer.valueOf(strArr[2]);
             }
+            return RowFactory.create(name, height, weight);
         });
 
         StructType schema = new StructType(new StructField[] {
@@ -107,4 +105,5 @@ public class HbaseWriterTest {
         System.out.println("finish");
 
     }
+
 }
