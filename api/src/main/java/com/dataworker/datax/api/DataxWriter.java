8e7724a2da386888b52b2fa401efc798f1459121
huaixin <libinsong1204@gmail.com>
2021-12-10 18:00:32 +0800

    fix 解决序列化

--- api/src/main/java/com/dataworker/datax/api/DataxWriter.java
+++ api/src/main/java/com/dataworker/datax/api/DataxWriter.java
@@ -6,13 +6,14 @@ import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
 import java.io.IOException;
+import java.io.Serializable;
 import java.util.Map;
 
 /**
  * @author melin 2021/7/27 10:47 上午
  */
 @SPI
-public interface DataxWriter {
+public interface DataxWriter extends Serializable {
 
     void validateOptions(Map<String, String> options);
 
