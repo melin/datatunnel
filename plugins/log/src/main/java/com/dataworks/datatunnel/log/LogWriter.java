357cc80ead2fb3c008936e3b2f692f38ee59189c
huaixin <libinsong1204@gmail.com>
2022-02-09 18:33:42 +0800

    update

--- plugins/log/src/main/java/com/dataworks/datatunnel/log/LogWriter.java
+++ plugins/log/src/main/java/com/dataworks/datatunnel/log/LogWriter.java
@@ -1,10 +1,11 @@
 package com.dataworks.datatunnel.log;
 
 import com.dataworks.datatunnel.api.DataxWriter;
-import com.dataworker.spark.jobserver.api.LogUtils;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.Map;
@@ -13,6 +14,8 @@ import java.util.Map;
  * @author melin 2021/7/27 11:06 上午
  */
 public class LogWriter implements DataxWriter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LogWriter.class);
 
     @Override
     public void validateOptions(Map<String, String> options) {
@@ -25,6 +28,6 @@ public class LogWriter implements DataxWriter {
         int truncate = Integer.valueOf(options.getOrDefault("truncate", "20"));
         boolean vertical = Boolean.valueOf(options.getOrDefault("vertical", "false"));
         String data = dataset.showString(numRows, truncate, vertical);
-        LogUtils.stdout(sparkSession, data);
+        LOGGER.info(data);
     }
 }
