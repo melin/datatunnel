ab48b4f0d5e5ebedd848cc99ffe5e35d30339914
huaixin <libinsong1204@gmail.com>
2022-02-10 20:35:40 +0800

    update

--- plugins/kafka/src/main/scala/com/dataworks/datatunnel/kafka/reader/KafkaReader.scala
+++ plugins/kafka/src/main/scala/com/dataworks/datatunnel/kafka/reader/KafkaReader.scala
@@ -7,9 +7,11 @@ import com.dataworks.datatunnel.kafka.util.HudiUtils
 import com.gitee.melin.bee.util.MapperUtils
 import com.google.common.collect.Maps
 import org.apache.commons.lang3.StringUtils
-import org.apache.spark.sql.{Dataset, Row, SaveMode, SparkSession}
+import org.apache.spark.sql.streaming.{OutputMode, Trigger}
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SaveMode, SparkSession}
 
 import java.util
+import scala.concurrent.duration.DurationInt
 
 /**
  * huaixin 2021/12/29 2:23 PM
@@ -84,7 +86,23 @@ class KafkaReader extends DataxReader {
 
       val sql = CommonUtils.genOutputSql(dataset, options)
       dataset = sparkSession.sql(sql)
-      dataset.write.format("jdbc").mode(mode).option("url", url).option("dbtable", table).option("batchsize", batchsize).option("queryTimeout", queryTimeout).option("truncate", truncate).option("user", username).option("password", password).save()
+      val query= dataset.writeStream.trigger(Trigger.ProcessingTime(10.seconds))
+        .outputMode(OutputMode.Update)
+        .foreachBatch { (batchDF: DataFrame, batchId: Long) =>
+          batchDF.write
+            .format("jdbc")
+            .mode(mode)
+            .option("url", url)
+            .option("dbtable", table)
+            .option("batchsize", batchsize)
+            .option("queryTimeout", queryTimeout)
+            .option("truncate", truncate)
+            .option("user", username)
+            .option("password", password)
+            .save
+        }.start()
+
+      query.awaitTermination()
     } else {
       throw new UnsupportedOperationException("kafka 数据不支持同步到 " + sinkType)
     }
