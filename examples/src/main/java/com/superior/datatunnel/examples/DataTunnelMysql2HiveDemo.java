ed0ded424b22f2f48a181588a66397e148f637e7
melin <libinsong1204@gmail.com>
2023-03-05 20:10:50 +0800

    add example: mysql to hive

---
+++ examples/src/main/java/com/superior/datatunnel/examples/DataTunnelMysql2HiveDemo.java
@@ -0,0 +1,37 @@
+package com.superior.datatunnel.examples;
+
+import com.superior.datatunnel.core.DataTunnelExtensions;
+import org.apache.spark.sql.SparkSession;
+
+public class DataTunnelMysql2HiveDemo {
+
+    public static void main(String[] args) {
+        SparkSession spark = SparkSession
+                .builder()
+                .enableHiveSupport()
+                .master("local")
+                .appName("Iceberg spark example")
+                .config("spark.metrics.staticSources.enabled", false)
+                //.config("spark.sql.parquet.compression.codec", "zstd")
+                .config("spark.sql.sources.partitionOverwriteMode", "dynamic")
+                .config("spark.sql.extensions", DataTunnelExtensions.class.getName())
+                .getOrCreate();
+
+        String sql = "datatunnel SOURCE('mysql') OPTIONS(\n" +
+                "    username='root',\n" +
+                "    password='root2023',\n" +
+                "    host='172.18.5.44',\n" +
+                "    port=3306,\n" +
+                "    resultTableName='temp_meta_job',\n" +
+                "    condition=\"type='spark_sql' and 1=1\", \n" +
+                "    databaseName='superior', tableName='meta_job', columns=['*'])\n" +
+                //"    TRANSFORM = 'select * from temp_meta_job where type=\"spark_sql\"'\n" +
+                "SINK('hive') OPTIONS(" +
+                "   databaseName = 'bigdata'," +
+                "   tableName = 'hive_meta_job'," +
+                "   columns = ['*']" +
+                ")";
+
+        spark.sql(sql);
+    }
+}
