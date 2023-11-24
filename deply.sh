67950a18ef5771639c78271ef8ab6b198ecd0d2e
melin <libinsong1204@gmail.com>
2023-11-24 13:33:16 +0800

    update deploy.sh

--- deply.sh
+++ deply.sh
@@ -1,3 +1,9 @@
 scp assembly/target/datatunnel-3.4.0.tar.gz root@172.18.5.46:/root
 123caoqwe
 
+rm -rf datatunnel-3.4.0
+tar -zxf datatunnel-3.4.0.tar.gz
+cd datatunnel-3.4.0
+hdfs dfs -rm /user/superior/spark-jobserver/datatunnel-3.4.0/*
+hdfs dfs -put *.jar /user/superior/spark-jobserver/datatunnel-3.4.0/
+
