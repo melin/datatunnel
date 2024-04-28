bb0ffaae17cc711f5474cefbcc7c36c83f076065
melin <libinsong1204@gmail.com>
2024-04-28 20:11:20 +0800

    add log

--- connectors/redshift/src/main/java/com/superior/datatunnel/plugin/redshift/RedshiftDataTunnelSink.java
+++ connectors/redshift/src/main/java/com/superior/datatunnel/plugin/redshift/RedshiftDataTunnelSink.java
@@ -70,6 +70,7 @@ public class RedshiftDataTunnelSink implements DataTunnelSink {
             sql = "DROP TABLE IF EXISTS " + dbtable + ";";
             postActions = ArrayUtils.add(postActions, sql);
             postActions = ArrayUtils.add(postActions, "END;");
+            LOGGER.info("postActions: {}", StringUtils.join(postActions, " "));
         } else if (writeMode == WriteMode.OVERWRITE) {
             dbtable = "\"" + schemaName + "\".\"" + tableName + "\"";
             // preactions
