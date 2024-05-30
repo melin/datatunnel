07d3601ee59e7581adb3df23093198aaedd9fd3b
melin <libinsong1204@gmail.com>
2024-05-30 13:12:53 +0800

    update

--- connectors/jdbc/src/main/java/com/superior/datatunnel/plugin/jdbc/JdbcDataTunnelSource.java
+++ connectors/jdbc/src/main/java/com/superior/datatunnel/plugin/jdbc/JdbcDataTunnelSource.java
@@ -34,7 +34,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 import static com.superior.datatunnel.api.DataSourceType.ORACLE;
 import static java.sql.Types.*;
