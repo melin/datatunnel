68d695c1854936a734e947c636b060188a781936
melin <libinsong1204@gmail.com>
2024-06-18 20:12:25 +0800

    fix jdbc
    (cherry picked from commit 72d778e6edcb2d5c0fe5ae610f5cd90250488949)

--- connectors/jdbc/src/main/java/com/superior/datatunnel/plugin/jdbc/JdbcDataTunnelSink.java
+++ connectors/jdbc/src/main/java/com/superior/datatunnel/plugin/jdbc/JdbcDataTunnelSink.java
@@ -55,6 +55,7 @@ public class JdbcDataTunnelSink implements DataTunnelSink {
         options.add("keytab");
         options.add("principal");
         options.add("refreshKrb5Config");
+        options.add("driver");
         return options;
     }
 
