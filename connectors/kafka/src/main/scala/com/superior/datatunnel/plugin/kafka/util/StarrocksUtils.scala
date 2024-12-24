51d97503982f1857101b6759fac9ae346f371cdb
melin <libinsong1204@gmail.com>
2024-12-24 09:17:07 +0800

    支持kafka 写入 starrocks

--- connectors/kafka/src/main/scala/com/superior/datatunnel/plugin/kafka/util/StarrocksUtils.scala
+++ connectors/kafka/src/main/scala/com/superior/datatunnel/plugin/kafka/util/StarrocksUtils.scala
@@ -3,6 +3,9 @@ package com.superior.datatunnel.plugin.kafka.util
 import com.superior.datatunnel.plugin.starrocks.StarrocksDataTunnelSinkOption
 import org.apache.spark.internal.Logging
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.streaming.Trigger
+
+import java.util.concurrent.TimeUnit
 
 object StarrocksUtils extends Logging {
 
@@ -15,6 +18,26 @@ object StarrocksUtils extends Logging {
       sinkOption: StarrocksDataTunnelSinkOption,
       querySql: String
   ): Unit = {
-    throw new RuntimeException("not support")
+    val streamingInput = spark.sql(querySql)
+
+    val writer = streamingInput.writeStream
+      .options(sinkOption.getProperties)
+
+    val fullTableId = sinkOption.getDatabaseName + "." + sinkOption.getTableName();
+    writer
+      .trigger(Trigger.ProcessingTime(triggerProcessingTime, TimeUnit.SECONDS))
+      .format("starrocks")
+      .options(sinkOption.getProperties())
+      .option("starrocks.fenodes", sinkOption.getFeEnpoints())
+      .option("starrocks.fe.jdbc.url", sinkOption.getJdbcUrl())
+      .option("user", sinkOption.getUsername())
+      .option("password", sinkOption.getPassword())
+      .option("starrocks.table.identifier", fullTableId)
+      .option("starrocks.columns", sinkOption.getColumns.mkString(","))
+      .option("checkpointLocation", checkpointLocation)
+
+    writer
+      .start()
+      .awaitTermination()
   }
 }
