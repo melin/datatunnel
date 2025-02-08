cfe68fb008963385ac9135f80051a7d92fe621ae
melin <libinsong1204@gmail.com>
2025-02-08 10:55:58 +0800

    add Mysql2DorisDemo.kt

---
+++ examples/src/main/kotlin/com/superior/datatunnel/examples/doris/Mysql2DorisDemo.kt
@@ -0,0 +1,45 @@
+package com.superior.datatunnel.examples.doris
+
+import com.superior.datatunnel.core.DataTunnelExtensions
+import org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions
+import org.apache.spark.sql.SparkSession
+
+object Mysql2DorisDemo {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+
+        val spark = SparkSession
+            .builder()
+            .master("local")
+            .appName("Datatunnel spark example")
+            .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+            .config(
+                "spark.sql.extensions",
+                DataTunnelExtensions::class.java.name + "," + IcebergSparkSessionExtensions::class.java.name
+            )
+            .getOrCreate()
+
+        val sql = """
+        DATATUNNEL SOURCE("mysql") OPTIONS (
+              username = "root",
+              password = "Root2024!@",
+              host = '172.18.6.181',
+              port = 3306,
+              databaseName = 'demos',
+              tableName = 'account',
+              columns = ["*"]
+        ) 
+        SINK("doris") OPTIONS (
+            feEnpoints="172.18.5.44:18030",
+            username = "root",
+            password = "root2024",
+            databaseName = "testdb",
+            tableName = "ods_account"
+        )
+        """.trimIndent()
+
+        spark.sql(sql)
+    }
+
+}
