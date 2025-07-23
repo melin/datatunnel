27955b82e57b1ae6f2b4d0eb64eb42ac6aa01c65
melin <libinsong1204@gmail.com>
2025-07-23 14:26:22 +0800

    fixbug

--- connectors/hive/src/main/java/com/superior/datatunnel/plugin/hive/HiveUtils.java
+++ connectors/hive/src/main/java/com/superior/datatunnel/plugin/hive/HiveUtils.java
@@ -1,16 +1,23 @@
 package com.superior.datatunnel.plugin.hive;
 
+import com.gitee.melin.bee.util.JsonUtils;
 import com.superior.datatunnel.api.DataTunnelException;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.catalyst.TableIdentifier;
 import org.apache.spark.sql.catalyst.catalog.CatalogTable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import scala.Option;
+import scala.collection.Map;
 
 /**
  * @author melin 2021/11/9 3:11 下午
  */
 public class HiveUtils {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HiveUtils.class);
 
     public static boolean checkPartition(SparkSession sparkSession, String databaseName, String tableName) {
         try {
@@ -18,7 +25,9 @@ public class HiveUtils {
                 TableIdentifier tableIdentifier = new TableIdentifier(tableName);
                 CatalogTable table = sparkSession.sessionState().catalog().getTableMetadata(tableIdentifier);
 
-                if (table.partitionColumnNames().size() > 0) {
+                String[] partitionNames = queryPartitionsColumns(table);
+                LOGGER.info("tableId: {}, partitionNames: {}", table, StringUtils.join(partitionNames, ","));
+                if (partitionNames != null && partitionNames.length > 0) {
                     return true;
                 }
             } else {
@@ -26,7 +35,10 @@ public class HiveUtils {
                 try {
                     TableIdentifier tableIdentifier = new TableIdentifier(tableName, Option.apply(databaseName));
                     CatalogTable table = sparkSession.sessionState().catalog().getTableMetadata(tableIdentifier);
-                    if (table.partitionColumnNames().size() > 0) {
+
+                    String[] partitionNames = queryPartitionsColumns(table);
+                    LOGGER.info("tableId: {}, partitionNames: {}", table, StringUtils.join(partitionNames, ","));
+                    if (partitionNames != null && partitionNames.length > 0) {
                         return true;
                     }
                 } finally {
@@ -34,9 +46,36 @@ public class HiveUtils {
                 }
             }
         } catch (Exception e) {
-            throw new DataTunnelException("检测表是否分区失败", e);
+            throw new DataTunnelException("检测表是否分区失败: " + e.getMessage(), e);
         }
 
         return false;
+    }
+
+    private static String[] queryPartitionsColumns(CatalogTable table) {
+        Map<String, String> properties = table.properties();
+        if (table.partitionColumnNames().size() > 0) {
+            return table.partitionColumnNames().mkString(",").split(",");
+        } else if (properties.contains("table_type")) {
+            String tableType = properties.get("table_type").get();
+            if (StringUtils.equals(tableType, "PAIMON")) {
+                if (properties.contains("partition")) {
+                    String partition = properties.get("partition").get();
+                    return StringUtils.split(partition, ",");
+                }
+            } else if (StringUtils.equals(tableType, "ICEBERG")) {
+                if (properties.contains("default-partition-spec")) {
+                    String json = properties.get("default-partition-spec").get();
+                    LinkedHashMap<String, Object> partitionSpec = JsonUtils.toJavaMap(json);
+
+                    ArrayList<LinkedHashMap<String, Object>> fields =
+                            (ArrayList<LinkedHashMap<String, Object>>) partitionSpec.get("fields");
+
+                    return fields.stream().map(map -> (String) map.get("name")).toArray(String[]::new);
+                }
+            }
+        }
+
+        return null;
     }
 }
