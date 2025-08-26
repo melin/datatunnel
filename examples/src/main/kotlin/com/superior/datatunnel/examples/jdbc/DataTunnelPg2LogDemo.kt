a946b272b3f8b0ffe52b32bff460e3181af33ef8
melin <libinsong1204@gmail.com>
2025-08-26 20:20:52 +0800

    fixbug pg

---
+++ examples/src/main/kotlin/com/superior/datatunnel/examples/jdbc/DataTunnelPg2LogDemo.kt
@@ -0,0 +1,36 @@
+package com.superior.datatunnel.examples.jdbc
+
+import com.superior.datatunnel.core.DataTunnelExtensions
+import org.apache.spark.sql.SparkSession
+
+object DataTunnelPg2LogDemo {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        System.setProperty("HADOOP_USER_NAME", "hdfs");
+
+        val spark = SparkSession
+            .builder()
+            .master("local")
+            .enableHiveSupport()
+            .appName("Datatunnel spark example")
+            .config("spark.sql.extensions", DataTunnelExtensions::class.java.name)
+            .getOrCreate()
+
+        val sql = """
+            DATATUNNEL SOURCE('postgresql') OPTIONS (
+                username = 'postgres',
+                password='123456' ,
+                host = '172.18.1.192',
+                port = 5532 ,
+                databaseName = 'postgres',
+                schemaName = 'public',
+                tableName = 'orders',
+                columns = ["id", "customer_id", "amount", "created_at"]
+            ) 
+            SINK("log")
+        """.trimIndent()
+
+        spark.sql(sql)
+    }
+}
