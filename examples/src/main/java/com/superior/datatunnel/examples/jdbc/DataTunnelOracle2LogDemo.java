ca2ae533b7a0f3a7db587f7a9c7d06dcc277e084
melin <libinsong1204@gmail.com>
2025-09-30 14:39:37 +0800

    update

--- examples/src/main/java/com/superior/datatunnel/examples/jdbc/DataTunnelOracle2LogDemo.java
+++ examples/src/main/java/com/superior/datatunnel/examples/jdbc/DataTunnelOracle2LogDemo.java
@@ -14,11 +14,10 @@ public class DataTunnelOracle2LogDemo {
                 .config("spark.sql.extensions", DataTunnelExtensions.class.getName())
                 .getOrCreate();
 
-        String sql = "datatunnel SOURCE('oracle') OPTIONS(\n" + "    username='flinkuser',\n"
+        String sql = "datatunnel SOURCE('oracle') OPTIONS(\n"
+                + "    username='flinkuser',\n"
                 + "    password='flinkpw',\n"
-                + "    host='172.18.1.56',\n"
-                + "    port=1521,\n"
-                + "    serviceName='XE',\n"
+                + "    jdbcUrl='jdbc:oracle:thin:@//172.18.1.51:1523/XE',\n"
                 + "    databaseName='FLINKUSER',"
                 + "    tableName='ORDERS', columns=['ORDER_ID', 'CUSTOMER_NAME', 'PRICE', 'PRODUCT_ID', 'ORDER_STATUS'])\n"
                 + "    SINK('log') OPTIONS(numRows = 10)";
